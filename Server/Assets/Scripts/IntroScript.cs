1800904becf6dd56d582408f8b997fe89070ec07
JakobDutch <boriskusters@gmail.com>
2023-04-18 12:12:15 +0200

    Added Intro

---
+++ Server/Assets/Scripts/IntroScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroScript : MonoBehaviour
+{
+    [SerializeField] List<GameObject> Slides;
+    [SerializeField] GameObject MainScreen;
+    int i = 0;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && i < Slides.Count)
+        {
+            Slides[i].SetActive(false);
+            i++;
+            Slides[i].SetActive(true);
+        }
+        if(i >= 5)
+        {
+            MainScreen.gameObject.SetActive(true);
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Server/Assets/Scripts/PlayButton.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayButton : MonoBehaviour
+{
+    public GameObject PlayScreen;
+    public void StartGame()
+    {
+        GameObject StartScreen = GameObject.Find("StartScreen");
+        StartScreen.SetActive(false);
+
+        //GameObject PlayScreen = GameObject.Find("PlayScreen");
+        PlayScreen.SetActive(true);
+    }
+    public void StartLevelOne()
+    {
+    }
+}
